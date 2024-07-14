488fe1cd696a5c7904fc8ec5146ecafce3f8e80a
Naol Basaye <57215685+NaolGB@users.noreply.github.com>
2024-07-14 04:01:29 +0100

    feat: Add tenant foreign key to Workspace model

--- api/workspace/models.py
+++ api/workspace/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from tenant.models import Tenant
 
 class Workspace(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -7,6 +8,7 @@ class Workspace(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
     activeVersion = models.CharField(max_length=255, null=True, blank=True)
+    tenant = models.ForeignKey(Tenant, on_delete=models.PROTECT, null=False, blank=False)
     
     def __str__(self):
         return self.displayName
---
+++ api/workspace/serializers.py
@@ -0,0 +1,18 @@
+from django.db import transaction
+from rest_framework import serializers
+from user.models import CustomUser
+from tenant.models import Tenant
+from workspace.models import Workspace
+
+class WorkspaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workspace
+        fields = ["displayName"]
+    
+    def create(self, validated_data):
+        with transaction.atomic():
+        # get the tenant from user.tenant
+            tenant = self.context['request'].user.tenant
+            instance = self.Meta.model(**validated_data, tenant=tenant)
+            instance.save()
+            return instance
