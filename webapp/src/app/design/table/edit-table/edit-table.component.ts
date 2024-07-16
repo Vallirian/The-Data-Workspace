5a732c3433f51cc15ffc2e7cc9fc25ec5a64a743
Naol Basaye <57215685+NaolGB@users.noreply.github.com>
2024-07-16 21:14:48 +0100

    chore: Update edit-table component layout and styles

--- webapp/src/app/design/table/edit-table/edit-table.component.ts
+++ webapp/src/app/design/table/edit-table/edit-table.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edit-table',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './edit-table.component.html',
   styleUrl: './edit-table.component.scss'
 })
 export class EditTableComponent {
+  selectedSection: 'section-details' | 'section-columns' | 'section-automations' = 'section-details';
 
+  constructor() { }
 }
