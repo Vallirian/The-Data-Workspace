d31c2ba2304342ad710de66d3e5dca3ed8d0c9c0
Naol Basaye <57215685+NaolGB@users.noreply.github.com>
2024-10-29 16:53:03 +0000

    Refactor database configuration to use PostgreSQL instead of MySQL

--- api/api/settings.py
+++ api/api/settings.py
@@ -95,15 +95,12 @@ WSGI_APPLICATION = "api.wsgi.application"
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DATABASE'),
-        'USER': 'root', # has to be root becase we are accessing different schemas
-        'PASSWORD': os.getenv('MYSQL_ROOT_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', '3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432')
     }
 }
 
