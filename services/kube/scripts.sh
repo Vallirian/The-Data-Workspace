1bb88c6f28d1eef88acde14284b52b343af2d2b2
Naol Basaye <57215685+NaolGB@users.noreply.github.com>
2024-10-31 22:26:12 +0000

    Add production configuration for API and webapp; update scripts for EKS and Minikube deployment

--- services/kube/scripts.sh
+++ services/kube/scripts.sh
@@ -1,15 +1,25 @@
+docker
+------
+docker inspect api:latest | grep Architecture # check image architecture
+
 reset minikube
 --------------
 minikube stop
 minikube delete
 minikube start --cpus=2 --memory=4000 --network-plugin=cni --cni=calico
 
+eks
+---
+eksctl create cluster -f cluster.yaml
+kubectl apply -f namespace.yaml
+
+
 open minikube dashboard
 -----------------------
 minikube dashboard
 
-create images
--------------
+local create images
+-------------------
 eval $(minikube docker-env)
 
 cd api/
@@ -17,6 +27,23 @@ docker build --no-cache -t api:latest .
 
 cd ../webapp/
 docker build --no-cache -t webapp:latest .
+
+prod create images
+------------------
+cd api/
+docker buildx build --platform linux/amd64 --no-cache -t 975050343564.dkr.ecr.eu-west-2.amazonaws.com/api:latest --push .
+
+
+cd ../webapp/
+docker buildx build --platform linux/amd64 --no-cache -t 975050343564.dkr.ecr.eu-west-2.amazonaws.com/webapp:latest --push .
+
+ecr login
+---------
+ECR_PASSWORD=$(aws ecr get-login-password --region eu-west-2)
+kubectl create secret docker-registry aws-ecr-reg-cred \
+  --docker-server=<account-id>.dkr.ecr.eu-west-2.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password="$ECR_PASSWORD"
 
 
 apply all
@@ -30,6 +57,10 @@ minikube service webapp -n dev
 
 K8s commands
 ------------
+aws eks --region eu-west-2 update-kubeconfig --name my-cluster # update context
+kubectl config get-contexts # get all contexts
+kubectl config current-context # get current context
+
 kubectl get pods --all-namespaces
 kubectl get pods -o wide
 kubectl describe pod [pod-name]
